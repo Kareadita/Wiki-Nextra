628284ba0e6c93e96616ded32e8e7f752706c3e3
DieselTech <30128380+DieselTech@users.noreply.github.com>
2024-02-01 18:36:58 -0500

    Update Head.tsx
    Changing social media card image

--- components/Head.tsx
+++ components/Head.tsx
@@ -18,10 +18,10 @@ export default function Head() {
 				property="og:description"
 				content="Lightning fast with a slick design, Kavita is a rocket fueled self-hosted digital library which supports a vast array of file formats."
 			/>
-			<meta property="og:url" content="https://stumpapp.dev" />
+			<meta property="og:url" content="https://www.kavitareader.com/" />
 			<meta property="og:type" content="website" />
-			<meta property="og:image" content="https://stumpapp.dev/og.png" />
-			<meta property="og:image:alt" content="Stump OG Image" />
+			<meta property="og:image" content="https://raw.githubusercontent.com/Kareadita/Wiki-Nextra/main/public/homepage-dark.png" />
+			<meta property="og:image:alt" content="OG Image" />
 			<meta property="og:image:type" content="image/png" />
 			<meta property="og:image:width" content="1332" />
 			<meta property="og:image:height" content="699" />
