01dff9a3a0c33df6d9a63237a469aa513db12a68
DieselTech <30128380+DieselTech@users.noreply.github.com>
2024-02-01 17:59:04 -0500

    Update nextra config
    Updating config to allow publishing to github pages.

--- next.config.js
+++ next.config.js
@@ -3,21 +3,30 @@ const withNextra = require('nextra')({
 	themeConfig: './theme.config.tsx',
 })
 
-const nextConfig = {
-  output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
+const isProduction = process.env.NODE_ENV === "production";
+const assetPrefix = isProduction ? "/wiki" : "";
 
-module.exports = {
+const nextConfig = {
   images: {
     unoptimized: true,
   },
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+  assetPrefix,
+  basePath: assetPrefix,
+  output: "export",
+};
+
+module.exports = {
+	...withNextra(),
+	async redirects() {
+		return [
+			{
+				destination: '/installation/getting-started',
+				permanent: true,
+				source: '/installation',
+			}
+		]
+	},
 }
