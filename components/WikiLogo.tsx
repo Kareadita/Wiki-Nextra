c1176d2c5f18d4d6cb4ba4cfa97d3ca7107b2260
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2024-11-15 12:05:52 -0600

    Build issue is still present, might need to adjust pipeline.

--- components/WikiLogo.tsx
+++ components/WikiLogo.tsx
@@ -1,7 +1,7 @@
 export default function WikiLogo() {
 	return (
 		<div className="flex items-center space-x-2">
-			<img className="h-8" src="/favicon.ico" alt="Kavita" />
+			<img className="h-6" src="/favicon.ico" alt="Kavita" />
 			<h3 className="text-xl font-bold text-gray-850 dark:text-gray-200">Kavita</h3>
 		</div>
 	)
--- next.config.mjs
+++ next.config.mjs
@@ -8,17 +8,12 @@ const withNextra = nextra({
 const isProduction = process.env.NODE_ENV === "production";
 const assetPrefix = isProduction ? "/wiki" : "";
 
-const nextConfig = {
-  reactStrictMode: true,
-  outputFileTracing: true,
-  swcMinify: true,
-  trailingSlash: true,
-  assetPrefix,
-  basePath: assetPrefix,
-  output: "export",
-};
-
 export default withNextra({
+	reactStrictMode: true,
+	trailingSlash: true,
+	assetPrefix,
+	basePath: assetPrefix,
+	output: "export",
 	images: {
 		unoptimized: true,
 		},
