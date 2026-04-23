ea06a1d7f224d94261dbccf443a33a069daca9ab
Joe Milazzo <josephmajora@gmail.com>
2026-04-23 16:00:46 -0500

    Started tweaking the filtering page.

--- pages/guides/_meta.js
+++ pages/guides/_meta.js
@@ -3,11 +3,11 @@ export default {
 	"user-settings": "User Settings",
 	"updating": "Updating",
 	"features": "Features",
-	"3rdparty": "3rd Party Clients",
-	"external-tools": "External Tools",
 	"metadata": "Metadata",
 	"readers": "Readers",
 	"scanner": "Library Scanner",
+	"3rdparty": "3rd Party Clients",
+	"external-tools": "External Tools",
 	"themes": "Custom Themes",
 	"api": "API"
 }
