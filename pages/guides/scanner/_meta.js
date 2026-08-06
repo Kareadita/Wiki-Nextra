0bd9df5e1370b1e21929850d7dfdf8ae49442e2d
Lucas Winther <lucwin@ece.au.dk>
2026-08-06 20:36:23 +0200

    Explicitly add missing .mdx files to the _meta for the sidebar.

--- pages/guides/scanner/_meta.js
+++ pages/guides/scanner/_meta.js
@@ -1,6 +1,10 @@
 export default {
 	"managefiles": "Managing Files",
+	"manga": "Manga",
 	"comic": "Comic (Flexible)",
 	"comicvine": "Comic",
+	"epub": "EPUB",
+	"pdf": "PDF",
+	"image": "Images",
 
 }
