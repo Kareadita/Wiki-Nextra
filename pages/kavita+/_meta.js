8be6a9f004735ad9b78d781baa3a43266634cd1c
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2026-08-15 10:29:42 -0500

    Added audit pages

--- pages/kavita+/_meta.js
+++ pages/kavita+/_meta.js
@@ -2,6 +2,7 @@ export default {
 	"license-management": "License Management",
 	"metadata-controls": "Metadata Controls",
 	"metadata": "Metadata",
+	"audit": "Audit",
 	"progress-sync": "Progress Sync",
 	"recs-ratings-reviews": "Recs, Ratings, & Reviews",
 	"smart-collections": "Smart Collections",
